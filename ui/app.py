dc62678ee9b6bfa80582770f6b8e4f9d05e08936
Jim <jrthom18@gmail.com>
2017-01-07 16:16:53 -0700

    Adjusting app.py for use with Facebook Messenger webhooks, full vocab, don't reverse inputs

--- ui/app.py
+++ ui/app.py
@@ -1,37 +1,97 @@
-from flask import Flask, render_template, request
-from flask import jsonify
-
-app = Flask(__name__,static_url_path="/static") 
-
-#############
-# Routing
-#
-@app.route('/message', methods=['POST'])
-def reply():
-    return jsonify( { 'text': execute.decode_line(sess, model, enc_vocab, rev_dec_vocab, request.form['msg'] ) } )
-
-@app.route("/")
-def index(): 
-    return render_template("index.html")
-#############
-
-'''
-Init seq2seq model
-
-    1. Call main from execute.py
-    2. Create decode_line function that takes message as input
-'''
-#_________________________________________________________________
+import os
+import os.path
 import sys
-import os.static_url_path
+import json
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 import tensorflow as tf
 import execute
 
+import requests
+from flask import Flask, request
+
+app = Flask(__name__)
+
+# Facebook Messenger app verification
+VERIFY_TOKEN = "my_voice_is_my_password_verify_me"
+PAGE_ACCESS_TOKEN = "EAAUCWBAyvEkBAM5kmECcUmqxH15u8bKbGuhmJ1WNXZBDZBfOPlGESoPZC8KQvKqa9oelFuZCQ3REcCLxfX5tiqTRKZCWEfnAszhlCLZCAgVWZCmZA78IW0wj9yTe3fH46Qq0mcD0FZB4wfsydRg0cB4dAu6DY5HCE69ZAJ14JY4EFZCZAgZDZD"
+
 sess = tf.Session()
 sess, model, enc_vocab, rev_dec_vocab = execute.init_session(sess, conf='seq2seq_serve.ini')
-#_________________________________________________________________
 
-# start app
-if (__name__ == "__main__"): 
-    app.run(host='0.0.0.0', debug = False) 
+@app.route('/', methods=['GET'])
+def verify():
+    # when the endpoint is registered as a webhook, it must echo back
+    # the 'hub.challenge' value it receives in the query arguments
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == VERIFY_TOKEN:
+            return "Verification token mismatch", 403
+        return request.args["hub.challenge"], 200
+
+    return "Hello world", 200
+
+
+@app.route('/', methods=['POST'])
+def webhook():
+
+    # endpoint for processing incoming messaging events
+
+    data = request.get_json()
+    #log(data)  # you may not want to log every incoming message in production, but it's good for testing
+
+    if data["object"] == "page":
+
+        for entry in data["entry"]:
+            for messaging_event in entry["messaging"]:
+
+                if messaging_event.get("message"):  # someone sent us a message
+
+                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
+                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+                    message_text = messaging_event["message"]["text"]  # the message's text
+                    # Respond with decoded message
+                    send_message(sender_id, execute.decode_line(sess, model, enc_vocab, rev_dec_vocab, message_text.lower()))
+
+                if messaging_event.get("delivery"):  # delivery confirmation
+                    pass
+
+                if messaging_event.get("optin"):  # optin confirmation
+                    pass
+
+                if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
+                    pass
+
+    return "ok", 200
+
+
+def send_message(recipient_id, message_text):
+
+    log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
+
+    params = {
+        "access_token": PAGE_ACCESS_TOKEN
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "text": message_text
+        }
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        log(r.status_code)
+        log(r.text)
+
+
+def log(message):  # simple wrapper for logging to stdout on heroku
+    print str(message)
+    sys.stdout.flush()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
